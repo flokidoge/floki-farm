202d821a92d7174b1ba38929b797a503d27d22b1
flokimusk <86514345+flokimusk@users.noreply.github.com>
2021-06-28 13:51:33 +0800

    Add Floki Time Locker

--- contracts/FlokiLocker.sol
+++ contracts/FlokiLocker.sol
@@ -8,11 +8,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FlokiLocker is Ownable {
     using SafeBEP20 for IBEP20;
+    uint256 public endBlock = 0;
+    bool public closed = false;
 
     event Unlocked(address indexed token, address indexed recipient, uint256 amount);
 
+    function setEndBlock(uint256 _endBlock) public onlyOwner {
+        require(closed == false, "FlokiLocker::setEndBlock: closed");
+        endBlock = _endBlock;
+    }
+
+    function setClose() public onlyOwner {
+        closed = true;
+    }
+
     function unlock(IBEP20 _token, address _recipient) public onlyOwner {
         require(_recipient != address(0), "FlokiLocker::unlock: ZERO address.");
+        require(block.number > endBlock, "FlokiLocker::unlock: Before end");
 
         uint256 amount = _token.balanceOf(address(this));
         _token.safeTransfer(_recipient, amount);
